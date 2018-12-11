60c5f2b3796f7f5a91558507c44d0783e7cd84a9
Ramprasad Rajendran <r.ramprasad@gmail.com>
2018-12-11 18:27:10 +0530

    Basebacking class optimization
    Changed from prototype properties to instant properties
    Changes incorporated from https://github.com/gameclosure/timestep/pull/123/files
    
    Issues:
    Linear layout issues seem to be present

--- src/ui/backend/BaseBacking.js
+++ src/ui/backend/BaseBacking.js
@@ -24,44 +24,27 @@ var BaseBacking = exports = Class(function () {
   // this._onResize = function () {};
   // this._onZIndex = function () {};
 
-  var styleKeys = this.constructor.styleKeys = {};
-
-  // keys map to properties
-  var BASE_STYLE_PROPS = {
-    'x': {value: 0},
-    'y': {value: 0},
-    'offsetX': {value: 0}, //translate
-    'offsetY': {value: 0},
-    'offsetXPercent': {value: undefined, cb: '_onOffsetX'}, //not implemented
-    'offsetYPercent': {value: undefined, cb: '_onOffsetY'},
-    'anchorX': {value: 0}, //rotation and scale
-    'anchorY': {value: 0},
-    'centerAnchor': {value: false},
-    'width': {cb: '_onResize'},
-    'height': {cb: '_onResize'},
-    'r': {value: 0},
-    'opacity': {value: 1},
-    'zIndex': {value: 0, cb: '_onZIndex'},
-    'scale': {value: 1},
-    'scaleX': {value: 1},
-    'scaleY': {value: 1},
-    'flipX': {value: false},
-    'flipY': {value: false},
-    'visible': {value: true},
-    'shadowColor': {value: 'black'}, //only has an effect in TextView??
-    'clip': {value: false},
-    'backgroundColor': {value: ''},
-    'compositeOperation': {value: undefined},
-  };
-
-  this.constructor.addProperty = function (key, def) {
-    styleKeys[key] = true;
-    setProperty(this.prototype, key, def);
-  }
-
-  for (var key in BASE_STYLE_PROPS) {
-    this.constructor.addProperty(key, BASE_STYLE_PROPS[key]);
-  }
+  this.x = 0;
+  this.y = 0;
+  this.offsetX = 0;
+  this.offsetY = 0;
+  this.anchorX = 0;
+  this.anchorY = 0;
+  this.centerAnchor = 0;
+  this._width = 0;
+  this._height = 0;
+  this.r = 0;
+  this.opacity = 1;
+  this._zIndex = 0;
+  this.scale = 1;
+  this.scaleX = 1;
+  this.scaleY = 1;
+  this.flipX = false;
+  this.flipY = false;
+  this.visible = true;
+  this.clip = false;
+  this.backgroundColor = '';
+  this.compositeOperation = '';
 
   this.localizePoint = function (pt) {
     pt.x -= this.x + this.anchorX + this.offsetX;
@@ -73,21 +56,88 @@ var BaseBacking = exports = Class(function () {
     return pt;
   }
 
+  Object.defineProperty(this, 'width', {
+    get: function () {
+      return this._width;
+    },
+    set: function (width) {
+      if (this._width === width) {
+        return;
+      }
+      this._width = width;
+      this._onResize();
+    }
+  });
+
+  Object.defineProperty(this, 'height', {
+    get: function () {
+      return this._height;
+    },
+    set: function (height) {
+      if (this._height === height) {
+        return;
+      }
+      this._height = height;
+      this._onResize();
+    }
+  });
+
+  Object.defineProperty(this, 'zIndex', {
+    get: function () {
+      return this._zIndex;
+    },
+    set: function (zIndex) {
+      if (this._zIndex === zIndex) {
+        return;
+      }
+      this._zIndex = zIndex;
+      this._onZIndex('zIndex', zIndex);
+    }
+  });
+
   this.copy = function () {
-    var copy = {};
-    for (var key in styleKeys) {
+    var copy = {
+      x: this.x,
+      y: this.y,
+      offsetX: this.offsetX,
+      offsetY: this.offsetY,
+      anchorX: this.anchorX,
+      anchorY: this.anchorY,
+      centerAnchor: this.centerAnchor,
+      width: this._width,
+      height: this._height,
+      r: this.r,
+      opacity: this.opacity,
+      zIndex: this._zIndex,
+      scale: this.scale,
+      scaleX: this.scaleX,
+      scaleY: this.scaleY,
+      flipX: this.flipX,
+      flipY: this.flipY,
+      visible: this.visible,
+      clip: this.clip,
+      backgroundColor: this.backgroundColor,
+      compositeOperation: this.compositeOperation
+    };
+    for (var key in extendedStylePropList) {
       copy[key] = this[key];
     }
 
     return copy;
   }
-  
+
   this.update = function (style) {
     for (var i in style) {
-      if (style.hasOwnProperty(i) && styleKeys.hasOwnProperty(i)) {
+      if(this[i] !== void 0) {
         this[i] = style[i];
       }
     }
     return this;
   }
 });
+
+var extendedStylePropList = [];
+BaseBacking.prototype.constructor.addProperty = function (key, def) {
+  extendedStylePropList.push(key);
+  setProperty(BaseBacking.prototype, key, def);
+};
